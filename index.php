1a0259311ec817aa6da743ebed7e294438fbb8f3
Kaisma <16726593+bviengineer@users.noreply.github.com>
2019-09-14 18:38:47 -0400

    Update index.php
    remove html doctype and header information

--- index.php
+++ index.php
@@ -3,16 +3,6 @@
     include('inc/quiz.php');
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Math Quiz: Addition</title>
-    <link href='https://fonts.googleapis.com/css?family=Playfair+Display:400,400italic,700,700italic' rel='stylesheet' type='text/css'>
-    <link rel="stylesheet" href="css/normalize.css">
-    <link rel="stylesheet" href="css/styles.css">
-</head>
-<body>
     <div class="container">
         <div id="quiz-box">
             <p class="breadcrumbs">Question <?php  echo $_SESSION['questionCounter'] + 1 ?> of <?php echo $totalQuestions; ?>
