10fd93804ff90639983441d1bf225a84d18bf577
Kaisma <16726593+bviengineer@users.noreply.github.com>
2019-09-14 21:09:56 -0400

    Update header.php
    Update comment and moved session variable logic out of file

--- inc/header.php
+++ inc/header.php
@@ -1,29 +1,7 @@
 <?php 
-	// Start PHP session
+	// Start PHP session and will also be started on every page where the header is directly or subsequently included 
 	session_start();
-	//session_destroy();
-
-	// Include questions array 
-	include('inc/questions.php'); 
-
-	// Stores the total number of questions; will auto update if quantity changes 
-	$totalQuestions = count($questions);
-
-	// Variable will keep track of the amount of times the quiz is played 
-	$numAttempts = 0;
-
-	// Session variable initialization  
-	if(!isset($_SESSION['questionCounter']) || $_SESSION['questionCounter'] >= ($totalQuestions-1) ) {
-			$_SESSION[questionCounter] = 0;
-			// $numAttempts += 1;
-			// shuffle($questions);
-
-	} else {
-			$_SESSION[questionCounter]++;
-	}
-
-	$_SESSION['userAnswer'] = filter_input(INPUT_POST, 'answer', FILTER_SANITIZE_NUMBER_INT); 
-  echo $_SESSION['userAnswer'];
+	//session_destroy(); 
 ?>
 <!DOCTYPE html>
 <html lang="en">
