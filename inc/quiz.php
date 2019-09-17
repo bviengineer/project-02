4162533ee10ce4c779d11fb837901d5b34d218a4
Kaisma <16726593+bviengineer@users.noreply.github.com>
2019-09-17 17:23:57 -0400

    Update quiz.php
    total correct answers configured as a session variable.

--- inc/quiz.php
+++ inc/quiz.php
@@ -1,69 +1,54 @@
 <?php
 /*
  * PHP Techdegree Project 2: Build a Quiz App in PHP
- *
- * These comments are to help you get started.
- * You may split the file and move the comments around as needed.
- *
- * You will find examples of formating in the index.php script.
- * Make sure you update the index file to use this PHP script, and persist the users answers.
- *
- * For the questions, you may use:
- *  1. PHP array of questions
- *  2. json formated questions
- *  3. auto generate questions
 */
 
 // Inclusion of header.php and questions.php files 
 include('header.php');
 include('questions.php');
 
-// Stores the total number of questions; variable will auto update if quantity changes 
-$totalQuestions = count($questions);
+// Will persist the questions across sessions for shuffling to be effective 
+//print_r($_SESSION['quizQuestions'] = $questions); 
 
-// Variable will keep track of the amount of times the quiz is played 
-$_SESSION['numAttempts'] = 0;
-
-// Will keep track of the total number of questions answered correctly & incorrectly
-$_SESSION['totalCorrectAns'] = 0;
-$_SESSION['totalIncorrectAns'] = 0;
-
+$_SESSION['questionsAsked'] = []; // Will keep track of the questions asked in an array 
+$_SESSION['numAttempts'] = 0; // Variable will keep track of the amount of times the quiz is played 
+$totalQuestions = count($questions); // Stores the total number of questions; variable will auto update if quantity changes
 
 // Session variable initialization  
 if(!isset($_SESSION['questionCounter']) || $_SESSION['questionCounter'] >= ($totalQuestions-1) ) {
-        $_SESSION['questionCounter'] = 0;
-        // $numAttempts += 1;
+    $_SESSION['questionCounter'] = 0;
+    $_SESSION['totalCorrectAns'] = 0; // Will keep track of the total number of questions answered correctly
+    $_SESSION['numAttempts'] = 0;
 
 } else {
         $_SESSION['questionCounter']++;
 }
 
-// Assigning filtered user answer & hidden input field values to variables
+// Assigning & filtering user answer & hidden input field values to variables to be used elsewhere 
 $userAnswer = filter_input(INPUT_POST, 'answer', FILTER_SANITIZE_NUMBER_INT);
 $correctAnswer = filter_input(INPUT_POST, 'id', FILTER_SANITIZE_NUMBER_INT);
-
 // Verifying user answer
+
 if (isset($userAnswer) && $userAnswer == $correctAnswer) {
-    $_SESSION['totalCorrectAns']++;
     echo "<strong>CONGRATULATIONS!</strong> You have a <strong>total</strong> of " .  $_SESSION['totalCorrectAns'] . " correct answers!";
     //echo "it's a match: " . $userAnswer;   
 } else {
-    $_SESSION['totalIncorrectAns'] += 1;
-    echo "<strong>PLEASE TRY AGAIN!</strong> You have a <strong>total</strong> of " . $_SESSION['totalIncorrectAns'] . " incorrect answer!";
+    //$_SESSION['totalCorrectAns'] -= 1;
+   echo "<strong>PLEASE TRY AGAIN!</strong> That was incorrect!";
     //echo "It's not a match. The correct ans was: " . $correctAnswer;
 }
 
 // Show random question
 
+
 // Array of a single question will be presented to the quiz taker 
 $questionToAsk = $questions[$_SESSION['questionCounter']];
-
+//print_r($questionToAsk);
 
 // Keep track of which questions have been asked
-$questionsAsked = [];
-array_push($questionsAsked, $questionToAsk);
-// echo "<br> these are the questions asked thus far. <br>";
-// print_r($questionsAsked);
+//array_push($_SESSION['questionsAsked']);
+//array_push($questionsAsked, $questionToAsk);
+//print_r($questionsAsked);
 
 // Show which question they are on
     /// this logic is in the index.php file
