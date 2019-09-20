b0d39ffc4c6d345c0008c645996d387083ba6905
Kaisma <16726593+bviengineer@users.noreply.github.com>
2019-09-20 01:41:11 -0400

    Update quiz.php
    shuffle method reimplementation

--- inc/quiz.php
+++ inc/quiz.php
@@ -10,11 +10,13 @@
 include('header.php');
 include('questions.php');
 
-shuffle($questions);
+
+//QUESTION: are the questions being resassigned to the session variable each time the question changes?
 $_SESSION['randomQuestions'] = $questions; // To hold the array of questions; questions will be shuffled 
 //$_SESSION['questionsAsked']; // Will track an array of the questions asked
 $_SESSION['numAttempts'] = 0; // To keep track of the amount of times the quiz is played 
 $totalQuestions = count($questions); // Stores total # of questions; var will auto update if quantity changes
+
 
 // Counter Session variable initialization  
 if(!isset($_SESSION['questionCounter']) || $_SESSION['questionCounter'] >= ($totalQuestions-1) ) {
@@ -26,9 +28,23 @@ if(!isset($_SESSION['questionCounter']) || $_SESSION['questionCounter'] >= ($tot
         $_SESSION['questionCounter']++;
 }
 
+// Array of a single question will be presented to the quiz taker 
+if ($_SESSION['questionCounter'] > count($totalQuestions-1) ) {
+    shuffle($_SESSION['randomQuestions']);
+    $_SESSION['questionsAsked'] = [];
+}
+
+// for ($q = 1; $q <= $_SESSION['randomQuestions']; $q++){
+//     $_SESSION['randomQuestions'][$_SESSION['questionCounter']];
+//     array_push($_SESSION['questionsAsked'], $questionToAsk);
+//     var_dump($_SESSION['questionsAsked']);
+// }
+
+
 // Assigning & filtering user answer & hidden input field values to variables to be used elsewhere 
 $userAnswer = filter_input(INPUT_POST, 'answer', FILTER_SANITIZE_NUMBER_INT);
 $correctAnswer = filter_input(INPUT_POST, 'id', FILTER_SANITIZE_NUMBER_INT);
+
 
 // Verifying user answer & totaling num of correct answers
 if (isset($userAnswer) && $userAnswer == $correctAnswer) {
@@ -46,13 +62,9 @@ if (isset($userAnswer) && $userAnswer == $correctAnswer) {
     // present question to user on index page from session variable
     //verify that the same question is not being repeated by comparing the questions already asked to the question currently being asked. 
 
-// Array of a single question will be presented to the quiz taker 
-$questionToAsk = $_SESSION['randomQuestions'][$_SESSION['questionCounter']];
-
-// Show which question they are on
-    /// this logic is in the index.php file
 
 // Shuffle answer buttons
+
 
 
 // Toast correct and incorrect answers
