db0f12961fc718e2c6f2d54f76ad3d5ef1951b26
Kaisma <16726593+bviengineer@users.noreply.github.com>
2019-09-23 22:19:54 -0400

    Update quiz.php
    -Update, remove, relocate comments.
    - Implement final total

--- inc/quiz.php
+++ inc/quiz.php
@@ -13,21 +13,23 @@ include('questions.php');
 
 // Variables
 $_SESSION['randomQuestions'] = $questions; // To hold the array of questions; questions will be shuffled 
-$_SESSION['finalScore'] = 0; // To hold final quiz score
 $_SESSION['answerButtons'] = []; // To store answer button values that will be shuffled 
 $totalQuestions = count($questions); // Stores total # of questions; var will auto update if quantity changes
 
 
-// Counter Session variable initialization 
+// Counter Session variable initialization & session variable resets 
 if(!isset($_SESSION['questionCounter']) || $_SESSION['questionCounter'] >= ($totalQuestions - 1) ) {
-    $_SESSION['questionCounter'] = 0; // Will track which question is currently being displayed 
-    $_SESSION['totalCorrectAns'] = 0; // Will keep track of the total number of questions answered correctly
-    $_SESSION['finalScore'] = 0; // Resets final score to 0 after all questions have been asked 
-    $_SESSION['questionsAsked'] = [];
-    shuffle($_SESSION['randomQuestions']); // shuffles the session variable containing the array of questions
+  
+  // Will print final score to the page
+  echo "<h1>Your final score is: " . $_SESSION['totalCorrectAns'] . ". You answered " . $_SESSION['totalCorrectAns'] . " / " . $totalQuestions . " questions correctly!</h1>";
+
+  $_SESSION['questionCounter'] = 0; // Will track which question is currently being displayed 
+  $_SESSION['totalCorrectAns'] = 0; // Will keep track of the total number of questions answered correctly
+  shuffle($_SESSION['randomQuestions']); // shuffles the session variable containing the array of questions
     
-    // Storing of the shuffled questions in a session variable that will hold the shuffled array in place until all questions have been asked
-    $_SESSION['shuffledQuestions'] = $_SESSION['randomQuestions']; 
+  // Storing of the shuffled questions in a session variable that will hold the shuffled array without reshuffling, until all questions have been asked
+  $_SESSION['shuffledQuestions'] = $_SESSION['randomQuestions']; 
+
 } else {
     $_SESSION['questionCounter']++;
 }
@@ -37,27 +39,26 @@ if(!isset($_SESSION['questionCounter']) || $_SESSION['questionCounter'] >= ($tot
 $questionToAsk = $_SESSION['shuffledQuestions'][$_SESSION['questionCounter']];
 
 
-// Assigning & filtering user answer & hidden input field values to variables to be used elsewhere 
+// Verifying user answer
+  // Assigning & filtering quiz-taker answer(s) & hidden input field values to variables 
 $userAnswer = filter_input(INPUT_POST, 'answer', FILTER_SANITIZE_NUMBER_INT); 
 $correctAnswer = filter_input(INPUT_POST, 'id', FILTER_SANITIZE_NUMBER_INT);
 
 
 // Toast correct and incorrect answers
-  // Verifying user answer & totaling num of correct answers
+   // Totaling num of correct answers
+  // Keep track of answers
 if (isset($userAnswer) && $userAnswer == $correctAnswer) {
     $_SESSION['totalCorrectAns']++;
-
-    // Keep track of answers
-    $_SESSION['finalScore'] += $_SESSION['totalCorrectAns'];
     echo "<h1><strong>CONGRATULATIONS!</strong> That's the correct answer!</h1>";
 } else {
    echo "<h1><strong>PLEASE TRY AGAIN!</strong> That was incorrect!</h1>";
-   $_SESSION['finalScore'] -= 1;
-}
+} 
+
 
 // Shuffle answer buttons
-  // Grab each answer result from array $_SESSION['shuffledQuestions'] or $questionToAsk
-  // Push only the answers into another array
+  // Grab each answer element only from $_SESSION['shuffledQuestions'] or $questionToAsk
+  // Push answers into dedicated array
   array_push($_SESSION['answerButtons'], $questionToAsk['correctAnswer'], $questionToAsk['firstIncorrectAnswer'], $questionToAsk['secondIncorrectAnswer']);
    
   // Shuffle the array of answers
@@ -67,11 +68,5 @@ if (isset($userAnswer) && $userAnswer == $correctAnswer) {
   $answer1 = $_SESSION['answerButtons'][0];
   $answer2 = $_SESSION['answerButtons'][1];
   $answer3 = $_SESSION['answerButtons'][2]; 
-
-
-// Show score
-if ($_SESSION['questionCounter'] >= ($totalQuestions - 1) ){
-  echo "<h1>Your final score is: " . $_SESSION['finalScore'] . " You answered " . $_SESSION['finalScore'] . " / " . $totalQuestions . " questions correctly!</h1>";
-} 
 
 ?>
